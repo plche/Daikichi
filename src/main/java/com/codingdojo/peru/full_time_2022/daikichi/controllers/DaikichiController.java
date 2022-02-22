2dafc3ecace172ef9740d338f5d42bd85eba439d
plche <che.percy.l@gmail.com>
2022-02-22 07:46:58 -0500

    Added support for @PathVariable

--- src/main/java/com/codingdojo/peru/full_time_2022/daikichi/controllers/DaikichiController.java
+++ src/main/java/com/codingdojo/peru/full_time_2022/daikichi/controllers/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.codingdojo.peru.full_time_2022.daikichi.controllers;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +21,17 @@ public class DaikichiController {
 	@RequestMapping(value = "/tomorrow", method = RequestMethod.GET)
 	public String handleDaikichiTomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping(value = "/travel/{city}", method = RequestMethod.GET)
+	public String handleDaikichiTravel(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@RequestMapping(value = "/lotto/{number}", method = RequestMethod.GET)
+	public String handleDaikichiLotto(@PathVariable("number") int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		} else return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 }
